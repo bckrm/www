2d6a7d9c1bb5eb5f204edc894c02f187b6f97855
Tom Kraak <tom@backroom.io>
2016-09-07 13:32:34 -0400

    :rocket: ready set go

---
+++ app.js
@@ -0,0 +1,24 @@
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('babel-preset-latest')
+const pageId = require('spike-page-id')
+
+module.exports = {
+  devtool: 'source-map',
+  matchers: {
+    html: '*(**/)*.sgr',
+    css: '*(**/)*.sss'
+  },
+  ignore: ['**/readme.md', '**/default.sgr', '**/layouts/*', '**/_*', '**/.*'],
+  reshape: (ctx) => {
+    return htmlStandards({
+      webpack: ctx,
+      locals: { pageId: pageId(ctx) }
+    })
+  },
+  postcss: (ctx) => {
+    return cssStandards({ webpack: ctx })
+  },
+  babel: { presets: [jsStandards] },
+  server: { open: false }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+## About
+
+Backroom creates deliberate brands for ambitious people. We believe great brands come from smarts, collaboration, art and iteration.
