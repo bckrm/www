dffc4f3ca092b92590254105bfa5991e00831721
Tom Kraak <tom@backroom.io>
2016-10-11 11:56:58 -0400

    add heros content type

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ module.exports = {
   },
   plugins: [new Records({
     addDataTo: locals,
-    posts: { url: 'https://cdn.contentful.com/spaces/5b0pqhu33f9d/entries?access_token=ff9a716e1b77a8b1f0cf83df5eea3ab94b1051a6ad0ac65309a39e3f506decf9&content_type=2wKn6yEnZewu2SCCkus4as' }
+    posts: { url: 'https://cdn.contentful.com/spaces/5b0pqhu33f9d/entries?access_token=ff9a716e1b77a8b1f0cf83df5eea3ab94b1051a6ad0ac65309a39e3f506decf9&content_type=2wKn6yEnZewu2SCCkus4as' },
+    heros: { url: 'https://cdn.contentful.com/spaces/5b0pqhu33f9d/entries?access_token=ff9a716e1b77a8b1f0cf83df5eea3ab94b1051a6ad0ac65309a39e3f506decf9&content_type=hero' }
   })],
   postcss: (ctx) => {
     return cssStandards({ webpack: ctx })
