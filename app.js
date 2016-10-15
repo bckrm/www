0ba3da3ad772d554794e92efbc4d02f292125ee9
Tom Kraak <tom@backroom.io>
2016-10-15 17:05:17 -0400

    add testimonial to about page

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ module.exports = {
   plugins: [new Records({
     addDataTo: locals,
     posts: { url: 'https://cdn.contentful.com/spaces/5b0pqhu33f9d/entries?access_token=ff9a716e1b77a8b1f0cf83df5eea3ab94b1051a6ad0ac65309a39e3f506decf9&content_type=2wKn6yEnZewu2SCCkus4as' },
-    heros: { url: 'https://cdn.contentful.com/spaces/5b0pqhu33f9d/entries?access_token=ff9a716e1b77a8b1f0cf83df5eea3ab94b1051a6ad0ac65309a39e3f506decf9&content_type=hero&order=sys.createdAt' }
+    heros: { url: 'https://cdn.contentful.com/spaces/5b0pqhu33f9d/entries?access_token=ff9a716e1b77a8b1f0cf83df5eea3ab94b1051a6ad0ac65309a39e3f506decf9&content_type=hero&order=sys.createdAt' },
+    testimonials: { url: 'https://cdn.contentful.com/spaces/5b0pqhu33f9d/entries?access_token=ff9a716e1b77a8b1f0cf83df5eea3ab94b1051a6ad0ac65309a39e3f506decf9&content_type=testimonial&order=sys.createdAt' }
   })],
   postcss: (ctx) => {
     return cssStandards({ webpack: ctx })
